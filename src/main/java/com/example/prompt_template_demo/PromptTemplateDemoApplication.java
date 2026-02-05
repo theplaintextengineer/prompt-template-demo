ccde58400be13df5b4b743a75a6dbc29ea7f9ddf
The Plaintext Engineer <akshay2104.singh@gmail.com>
2026-02-05 11:00:17 +0000

    Initial Commit

---
+++ src/main/java/com/example/prompt_template_demo/PromptTemplateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.prompt_template_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptTemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptTemplateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prompt_template_demo/PromptTemplateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prompt_template_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptTemplateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
